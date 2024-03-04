f372ce175a6dcdf2d594a99db002d431fde2ab3b
Bahadır <82715842+bahadirAsilsoy@users.noreply.github.com>
2024-03-04 17:55:28 +0300

    düzenlemeler
    düzenlemeler

---
+++ 01_İlerlemeAdimlari/app.js
@@ -0,0 +1,32 @@
+const progress = document.getElementById("progress");
+const circles = document.querySelectorAll(".circle");
+const prev = document.getElementById("prev");
+const next = document.getElementById("next");
+
+let currentActive = 1;
+
+next.addEventListener("click", () => {
+    currentActive = Math.min(currentActive + 1, circles.length);
+    update();
+});
+
+prev.addEventListener("click", () => {
+    currentActive = Math.max(currentActive - 1, 1);
+    update();
+});
+
+function update() {
+    circles.forEach((circle, index) => {
+        if (index < currentActive) {
+            circle.classList.add('active');
+        } else {
+            circle.classList.remove('active');
+        }
+    });
+
+    const actives = document.querySelectorAll(".active");
+    progress.style.width = (actives.length - 1) / (circles.length - 1) * 100 + '%';
+
+    prev.disabled = currentActive === 1;
+    next.disabled = currentActive === circles.length;
+}
